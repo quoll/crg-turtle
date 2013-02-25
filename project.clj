f69b9cc7cb7086493504e32fada2ac6ac49aabea
Paul Gearon <gearon@computer.org>
2013-02-25 11:46:02 -0500

    bumped to 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject turtle "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject turtle "0.1.0"
+  :description "Turtle Parser for Clojure"
+  :url "https://github.com/quoll/crg-turtle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
