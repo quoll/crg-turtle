514f23775c11d640fb9696493a8bac40be117c76
Paul Gearon <gearon@computer.org>
2013-03-08 22:39:05 -0500

    First protocol in the move to full Clojure code

---
+++ src/main/pre/clj/crg/turtle/nodes.clj
@@ -0,0 +1,19 @@
+(ns crg.turtle.nodes)
+
+(defprotocol NodeBuilder
+  (new-blank [builder]
+             [builder ^String id]
+    "Creates a blank node")
+
+  (new-iri [builder ^String iri]
+           [builder ^String iri ^String base]
+           [builder ^Map prefix-map ^String base ^String prefix ^String local]
+    "Creates an IRI given a full IRI, or parts of one and a context")
+
+  (new-literal [builder ^String lexical type ^String lang]
+    "Creates a literal using a given lexical form, an IRI for the type (may be nil),
+     and a language code (only valid if the type indicates xsd:string or nil)"))
+
+(defprotocol MapFactory
+  (new-map [factory] "Creates a persistent map object"))
+
